4fefdc06e859dd17fa8616af4e2e75f049527673
НафНафДурикович <yana88yana99@gmail.com>
2024-04-07 22:19:12 +0300

    Задание 1-2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Task 1
+        System.out.println("\nTask1");
+        int userOperatingSystem = 1;
+        if (userOperatingSystem == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if  (userOperatingSystem == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Такая система не поддерживается");
+        }
+        //Task 2
+        System.out.println("\nTask2");
+        int operatingSystemYear = 2013;
+        if (userOperatingSystem == 1 && operatingSystemYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (userOperatingSystem == 1 && operatingSystemYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (userOperatingSystem == 0 && operatingSystemYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (userOperatingSystem == 0 && operatingSystemYear >= 2015) {
+            System.out.println("Установите  версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Такая система не поддерживается");
+        }
     }
 }
